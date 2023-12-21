d7552622740c1e09f8fbf8297762ab20f04cccf5
MustafaNisar2002 <131928563+MustafaNisar2002@users.noreply.github.com>
2023-12-21 01:41:17 -0500

    first main.tf test

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "ca-central-1"  # Change this to your desired AWS region
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "allow-http-ingress"
+  description = "Allow incoming traffic on port 80"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0a2e7efb4257c0907"  # Ubuntu 20.04 LTS
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my-ubuntu-instance"
+  }
+
+  user_data = <<-EOF
+            
+            sudo apt update
+            sudo apt install nginx
+            Y
+            sudo ufw allow 'Nginx HTTP'
+            sudo ufw enable
+            y
+            systemctl status nginx
+            curl -4 icanhazip.com
+            EOF
+
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+}
+
+output "public_ip" {
+  value = aws_instance.my_instance.public_ip
+  
+}
+
